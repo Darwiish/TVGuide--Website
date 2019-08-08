2d2187f12e201ca4be398f0355b2f9183bc6a935
abc <abc@example.com>
2019-08-08 02:04:44 +0200

    env file

--- express/DataAccess/DbConnection.js
+++ express/DataAccess/DbConnection.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const MONGO_URL = process.env.REACT_APP_MONGO_URL;
-mongoose.connect(
-  "mongodb+srv://admin:admin1@cluster0-jcmuy.mongodb.net/test?retryWrites=true",
-  { useNewUrlParser: true }
-);
 
-// mongoose.connect(MONGO_URL, {
-//   useNewUrlParser: true
-// });
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true
+});
 const connection = mongoose.connection;
 
 connection.once("open", function() {
